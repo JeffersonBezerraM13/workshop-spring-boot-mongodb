5a2772361eca2d67fa929aa075de23933867456e
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-07-03 15:19:07 -0300

    Conexão com MongoDB com Repository e Service

--- src/main/java/br/dcx/ufpb/jefferson/workshopmongo/resources/UserResource.java
+++ src/main/java/br/dcx/ufpb/jefferson/workshopmongo/resources/UserResource.java
@@ -1,27 +1,25 @@
 package br.dcx.ufpb.jefferson.workshopmongo.resources;
 
 import br.dcx.ufpb.jefferson.workshopmongo.domain.User;
+import br.dcx.ufpb.jefferson.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method= RequestMethod.GET)
     //@GetMapping -> mesma coisa que o de cima
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1","Maria Brown","maria@gmail.com");
-        User alex = new User("2","Alex Green","alex@gmail.com");
-        List<User> users = new ArrayList<>();
-        users.addAll(Arrays.asList(maria,alex));
-        return ResponseEntity.ok().body(users);
+        return ResponseEntity.ok().body(userService.findAll());
     }
 }
---
+++ src/main/java/br/dcx/ufpb/jefferson/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package br.dcx.ufpb.jefferson.workshopmongo.services;
+
+import br.dcx.ufpb.jefferson.workshopmongo.domain.User;
+import br.dcx.ufpb.jefferson.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    //mecanismo de injeção de depedencia do spring boot
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
